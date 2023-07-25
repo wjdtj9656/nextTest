a76c89e2cfbf94462dabfbf222f95bfea47a947b
wjdtj9656 <wjdtj9656@naver.com>
2023-07-26 06:47:46 +0900

    sample init

--- next-sample/src/app/sample/page.tsx
+++
@@ -1,4 +0,0 @@
-const Sample = () => {
-  return <span>샘플 페이지입니다.</span>
-}
-export default Sample;
---
+++ next-sample/src/pages/greet/[name].js
@@ -0,0 +1,15 @@
+export async function getServerSideProps({ params }) {
+  const { name } = params;
+  return {
+    props: {
+      name,
+    }
+  }
+}
+
+function Greet(props) {
+  return (
+    <h1>Hello, {props.name}!</h1>
+  )
+}
+export default Greet;
